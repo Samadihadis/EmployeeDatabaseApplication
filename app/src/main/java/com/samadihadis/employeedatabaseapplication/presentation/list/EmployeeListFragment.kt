cab6d3fdeecb15d89f5ca671292181184e340eea
Hadis Samadi <samadi.hadis@gmail.com>
2024-02-23 14:44:24 +0330

    Add intro , list and employee detail class for information employee

---
+++ app/src/main/java/com/samadihadis/employeedatabaseapplication/presentation/list/EmployeeListFragment.kt
@@ -0,0 +1,24 @@
+package com.samadihadis.employeedatabaseapplication.presentation.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.samadihadis.employeedatabaseapplication.databinding.FragmentEmployeeListBinding
+
+class EmployeeListFragment : Fragment() {
+    private lateinit var binding: FragmentEmployeeListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentEmployeeListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

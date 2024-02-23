b5a62f5f127f29471c2779cb994f2d9ac7fdf206
Hadis Samadi <samadi.hadis@gmail.com>
2024-02-23 15:43:42 +0330

    Add lifecycleScope to intro fragment class

--- app/src/main/java/com/samadihadis/employeedatabaseapplication/presentation/intro/IntroFragment.kt
+++ app/src/main/java/com/samadihadis/employeedatabaseapplication/presentation/intro/IntroFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.samadihadis.employeedatabaseapplication.databinding.FragmentIntroBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class IntroFragment : Fragment() {
     private lateinit var binding: FragmentIntroBinding
@@ -20,5 +24,11 @@ class IntroFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            delay(2000)
+            findNavController().navigate(
+                IntroFragmentDirections.actionToEmployeeListFragment()
+            )
+        }
     }
 }

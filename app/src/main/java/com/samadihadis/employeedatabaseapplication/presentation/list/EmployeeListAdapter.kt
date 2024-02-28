b4117b136580c5124f46b8a220e85208b8e8f431
Hadis Samadi <samadi.hadis@gmail.com>
2024-02-28 22:31:24 +0330

    Add set on click listener to adapter

--- app/src/main/java/com/samadihadis/employeedatabaseapplication/presentation/list/EmployeeListAdapter.kt
+++ app/src/main/java/com/samadihadis/employeedatabaseapplication/presentation/list/EmployeeListAdapter.kt
@@ -11,7 +11,7 @@ class EmployeeListAdapter(private val navController: NavController) :
     RecyclerView.Adapter<EmployeeItemListViewHolder>() {
 
     private var employeeList: MutableList<EmployeeModel> = mutableListOf()
-    private var employeeItemClickListener : ((EmployeeModel) -> Unit)? = null
+    private var employeeItemClickListener: ((EmployeeModel) -> Unit)? = null
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeeItemListViewHolder {
@@ -23,6 +23,7 @@ class EmployeeListAdapter(private val navController: NavController) :
     override fun getItemCount(): Int {
         return employeeList.size
     }
+
     fun addItemList(countryModelList: List<EmployeeModel>) {
         employeeList.addAll(countryModelList)
         notifyItemRangeInserted(employeeList.size - 1, countryModelList.size)
@@ -31,12 +32,13 @@ class EmployeeListAdapter(private val navController: NavController) :
     override fun onBindViewHolder(holder: EmployeeItemListViewHolder, position: Int) {
         holder.apply {
             personnelID.text = employeeList[position].personnelID.toString()
-            fullName.text = employeeList[position].firstName + " " +employeeList[position].lastName
+            fullName.text = employeeList[position].firstName + " " + employeeList[position].lastName
             rootLayout.setOnClickListener {
-
+                employeeItemClickListener?.invoke(employeeList[position])
             }
         }
     }
+
     fun setItemClickListener(listener: (EmployeeModel) -> Unit) {
         employeeItemClickListener = listener
     }

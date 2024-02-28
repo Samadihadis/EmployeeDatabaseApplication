79d1d85cf918d692e5853974b490916118d8f67c
Hadis Samadi <samadi.hadis@gmail.com>
2024-02-28 18:51:41 +0330

    Add add employee fragment

--- app/src/main/java/com/samadihadis/employeedatabaseapplication/presentation/add/AddEmployeeFragment.kt
+++ app/src/main/java/com/samadihadis/employeedatabaseapplication/presentation/add/AddEmployeeFragment.kt
@@ -1,4 +1,26 @@
 package com.samadihadis.employeedatabaseapplication.presentation.add
 
-class AddEmployeeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.samadihadis.employeedatabaseapplication.databinding.FragmentAddEmployeeBinding
+
+class AddEmployeeFragment() : Fragment() {
+    private lateinit var binding: FragmentAddEmployeeBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAddEmployeeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
